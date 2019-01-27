ff1187ca5b75aa31551ff4340a10c598c2591ec6
janarpeterson <jenku18@gmail.com>
2019-01-27 14:25:29 +0200

    adding jdbc support

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,16 +1,31 @@
 package com.luv2code.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
+@PropertySource("classPath:persistence-mysql.properties")
 public class DemoAppConfig  {
+
+    @Autowired
+    private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
 
     @Bean
     public ViewResolver viewResolver(){
@@ -19,5 +34,34 @@ public class DemoAppConfig  {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource securityDataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+        dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        dataSource.setUser(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.password"));
+
+        dataSource.setInitialPoolSize(getInt("connection.pool.initialPoolSize"));
+        dataSource.setMinPoolSize(getInt("connection.pool.minPoolSize"));
+        dataSource.setMaxPoolSize(getInt("connection.pool.maxPoolSize"));
+        dataSource.setMaxIdleTime(getInt("connection.pool.maxIdleTime"));
+
+        return dataSource;
+    }
+
+    private int getInt(String prop){
+        return Integer.parseInt(env.getProperty(prop));
     }
 }
